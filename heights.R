631beebcb562d1e435ae17a7cf8a3734d17281bb
TemTee <temmyteeliz@gmail.com>
2022-03-10 13:56:06 +0100

    First Commit

---
+++ heights.R
@@ -0,0 +1,10 @@
+# Load the data and libraries -----------------------------------------------------
+
+library(tidyverse)
+library(dplyr)
+library(dslabs)
+data(heights)
+options(digits = 3)
+
+# Data Exploration ----------------------------------------------------------------
+str(heights)
