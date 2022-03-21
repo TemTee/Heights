5298bf2c89aa59a9ff8aa3015ede87914717fe5d
TemTee <temmyteeliz@gmail.com>
2022-03-21 23:51:13 +0100

    up

--- heights.R
+++ heights.R
@@ -13,3 +13,12 @@ summary(heights)
 table(is.na(heights))
 table(heights$sex)
 plot(heights$sex, heights$height)
+heights %>% filter(sex == "Female") %>% 
+  summarise(avg = mean(height),
+            stnd_dv = sd(height),
+            quadrant = quantile(height, c(0,0.5,1)))
+
+heights %>% filter(sex == "Male") %>% 
+  summarise(avg = mean(height),
+            stnd_dv = sd(height),
+            quadrant = quantile(height, c(0,0.5,1)))
